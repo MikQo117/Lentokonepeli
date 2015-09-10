5a52284a2d532ba451e1359f0b873ece85c23ef6
Mikko Peltola <mikkelsson4@gmail.com>
2015-09-10 22:32:05 +0300

    Throttle control testing in progress

--- AirplaneGame/Assets/Scripts/PlaneController.cs
+++ AirplaneGame/Assets/Scripts/PlaneController.cs
@@ -3,28 +3,46 @@ using System.Collections;
 
 public class PlaneController : MonoBehaviour {
 
-    float maxForce;
+    public float maxForce;
     float maxNegForce;
     float lift;
     float throttle;
+    public float throttleDeadZone;
+    float yaw;
+    bool inControl;
+    Rigidbody2D rigidBody2D;
 
 	// Use this for initialization
 	void Start ()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    
-	}
+        //Movement
+        GetInput();
+        Move();
+        rigidBody2D.AddForce(transform.up * 10);
+    }
 
     void GetInput()
     {
         //Get throttle
         throttle = Input.GetAxis("Throttle");
-        //Get Yaw
+        //Get yaw
+        yaw = Input.GetAxis("Yaw");
+    }
 
+    void Move()
+    {
+        if (throttle > throttleDeadZone || throttle < -throttleDeadZone)
+        {
+            rigidBody2D.AddForce(transform.right * (throttle * maxForce));
+            
+        }
+        
+        Debug.Log(throttle);
     }
 }
